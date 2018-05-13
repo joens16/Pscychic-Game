9e42240f61bbc1b980a5728ce089b0983f7ac127
joens16 <joens16@hotmail.com>
2018-05-13 15:15:52 -0500

    Final changes before CSS

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,5 +1,3 @@
-// Letter choices that you can click and other variables set to 0
-
 var keyboardChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
@@ -13,17 +11,17 @@ function reset(){
     guessedLetters = [];
     document.getElementById("guesses_so_far").innerHTML="Your Guesses so far: "+guessedLetters;
     document.getElementById("guesses_left").innerHTML="Guesses left: "+attemptsLeft; 
-    //var computerGuess = keyboardChoices[Math.floor(Math.random() * keyboardChoices.length)]; 
-   // console.log(computerGuess);
+    computerGuess = keyboardChoices[Math.floor(Math.random() * keyboardChoices.length)]; 
+    console.log(computerGuess);
 }
-//var computerGuess = keyboardChoices[Math.floor(Math.random() * keyboardChoices.length)];
-//console.log(computerGuess);
+var computerGuess = keyboardChoices[Math.floor(Math.random() * keyboardChoices.length)];
+console.log(computerGuess);
 //Program will find random letter from keyboard
 
 //Computer will generate a random letter as user presses a letter
 document.onkeyup = function(event){
-    var computerGuess = keyboardChoices[Math.floor(Math.random() * keyboardChoices.length)];
-    console.log(computerGuess);
+   // var computerGuess = keyboardChoices[Math.floor(Math.random() * keyboardChoices.length)];
+   // console.log(computerGuess);
     var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
     console.log("MyKey "+pressedKey);
     guessedLetters.push(pressedKey);
@@ -31,7 +29,7 @@ document.onkeyup = function(event){
      //generate a random letter
     //console.log("comparación intentos restantes: "+attemptsLeft);
     if(attemptsLeft > 1){
-        //console.log(computerGuess + " yyyy "+ pressedKey);
+        console.log(computerGuess + " yyyy "+ pressedKey);
        
         if(computerGuess === pressedKey){
             //console.log("gano");
@@ -49,13 +47,20 @@ document.onkeyup = function(event){
 
     }else{
         //console.log("salto");
-        losses ++;
-        document.getElementById("losses").innerHTML="Losses: "+losses;
-        reset();
+        if(computerGuess === pressedKey){
+            //console.log("gano");
+            wins ++; 
+            document.getElementById("wins").innerHTML="Wins: "+wins;
+            //Call function to reset variables
+            
+            reset();
+        }else{
+            losses ++;
+            document.getElementById("losses").innerHTML="Losses: "+losses;
+            reset();
+        }
 
     }
 }
-
-
 
 
